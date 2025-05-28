22177f15eee68e1bd02fff89dc380eb694fa6ad4
nch303 <haoncse184899@fpt.edu.vn>
2025-05-28 22:49:24 +0700

    add mappings

---
+++ Application/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PreSchoolBE.src.Application.DTOs.Request;
+using PreSchoolBE.src.Infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterRequest, Account>();
+        }
+    }
+}
