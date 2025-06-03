74fc5a41eb4bda149dabab5ca9c1a2145f361343
Kawaguchi-Kou <ha.anh.khang@gmail.com>
2025-06-03 20:27:25 +0700

    implement cloudinary service

---
+++ Domain/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageAsync(Stream fileStream, string fileName);
+        Task<string> UploadVideoAsync(Stream fileStream, string fileName);
+    }
+
+}
